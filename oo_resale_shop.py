3c22ad82b98aa2617f8e440438caf3cce63551e7
Sarah Chavez <scchavez@smith.edu>
2023-02-10 09:10:22 -0500

    Resubmitting to Gradescope 2/10- buy and sell

--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -15,7 +15,6 @@ class ResaleShop:
     """
     def buy(self, computer: Computer):
         # 1. call Computer(...) constructor to create a new Computer instance
-        computer = Computer()
         # 2. call inventory.append(...) to add the new Computer instance to the inventory
         self.inventory.append(computer)
     
@@ -23,28 +22,34 @@ class ResaleShop:
     Sells a computer by removing the computer from the inventory.
     """
     def sell(self, computer: Computer):
-        computer = Computer()
-        self.inventory.remove(computer)
+        if computer in self.inventory:
+            self.inventory.remove(computer)
   
     """ 
     Prints all the items in the inventory (if it isn't empty). Prints error otherwise.
     """
     def print_inventory(self):
-        print(self.inventory)
+        for computer in self.inventory:
+            computer.print_details()
 
-    def main():
-        test_computer = Computer("Mac Pro (Late 2013)", "3.5 GHc 6-Core Intel Xeon E5", 1024, 64, "macOS Big Sur", 2013, 1500)
-        test_computer2 = Computer("Acer", "3.5 GHc 6-Core Intel Xeon E5", 512, 32, "Windows 10", 2020, 900)  
-    # COMPUTER CLASS: testing update price function
-        # test_computer.update_price(2000)
-        # test_computer.print_details()
-    # COMPUTER CLASS: testing update os function
-        # test_computer.update_os("new_macOS")
-        # test_computer.print_details()
-    # COMPUTER CLASS: testing refurbish function
-        # test_computer.refurbish()
-        # test_computer.print_details()
-    # RESALE SHOP CLASS: testing buy function
-    # RESALE SHOP CLASS: testing sell function
-        
-    main()
+def main():
+    test_computer = Computer("Mac Pro (Late 2013)", "3.5 GHc 6-Core Intel Xeon E5", 1024, 64, "macOS Big Sur", 2013, 1500)
+    test_computer2 = Computer("Acer", "3.5 GHc 6-Core Intel Xeon E5", 512, 32, "Windows 10", 2020, 900)  
+    myShop = ResaleShop()
+# COMPUTER CLASS: testing update price function
+    # test_computer.update_price(2000)
+    # test_computer.print_details()
+# COMPUTER CLASS: testing update os function
+    # test_computer.update_os("new_macOS")
+    # test_computer.print_details()
+# COMPUTER CLASS: testing refurbish function
+    # test_computer.refurbish()
+    # test_computer.print_details()
+# RESALE SHOP CLASS: testing buy function
+    myShop.buy(test_computer)
+    myShop.buy(test_computer2)
+    myShop.print_inventory()
+# RESALE SHOP CLASS: testing sell function
+    myShop.sell(test_computer)
+    myShop.print_inventory()
+main()
